aecf299c764b33f0f6188454ddcb9a8b634f8a2d
olya-woo <kiseleva54922@gmail.com>
2022-12-13 18:00:50 +0300

    Практическая работа

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -9,14 +9,27 @@ public class Calculator {
         Product product = new Product();
 
         while (true) {
-            System.out.println("Введите название товара");// ваш код начнется здесь
+            System.out.println("Введите название товара");
 
             product.name = scanner.next();
             listProduct = listProduct + product.name + "\n";
+            boolean isRightPrice = false;
+            while (!isRightPrice) {
+                System.out.println("Введите цену товара в формате рубли.копейки");
 
-            System.out.println("Введите цену товара в формате рубли.копейки");// ваш код начнется здесь
+                if (scanner.hasNextDouble()) {
+                    product.price = scanner.nextDouble();
 
-            product.price = scanner.nextDouble();
+                    if (product.price < 0) {
+                        System.out.println("Цена не может быть отрицательной");
+                    } else {
+                        isRightPrice = true;
+                    }
+                } else {
+                    System.out.println("Вы ввели не число!");
+                    scanner.next();
+                }
+            }
             sum = sum + product.price;
 
             System.out.println("Товар добавлен");
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,21 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int persons;
+        int persons = 5;
+        boolean isNumber = false;
 
-        while (true) {
+        while (!isNumber) {
             System.out.println("На скольких человек необходимо разделить счёт?");// ваш код начнется здесь
 
-            persons = scanner.nextInt();
-
-            if (persons == 1) {
-                System.out.println("Нет смысла ничего считать и делить");
-            } else if (persons < 1) {
-                System.out.println("Это некорректное значение для подсчёта");
+            if (scanner.hasNextInt()) {
+                persons = scanner.nextInt();
+                if (persons == 1) {
+                    System.out.println("Нет смысла ничего считать и делить");
+                } else if (persons < 1) {
+                    System.out.println("Это некорректное значение для подсчёта");
+                } else {
+                    isNumber = true;
+                }
             } else {
-                break;
+                System.out.println("Это неправильное число!");
+                scanner.next();
             }
         }
+
         Calculator calculator = new Calculator();
         calculator.sumProduct();
         System.out.println(calculator.listProduct);
