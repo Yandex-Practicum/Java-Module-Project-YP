b3cab1c69f0b0fba0803e37cbae186eb3e1dbc0e
Aleksandr Polyakov <master@artwist.ru>
2022-12-24 12:18:28 +0300

    ЧАСТЬ 2 // ПОКА НЕ РАБОТАЕТ КОРРЕКТНО // Добавил в Main.java перебор ввода с клавиатуры, чтобы собирать корректные названия и цены товаров.
    Требуется:
    — Внести в класс SplitTheBill атрибут вида List<FoodElement> billElementList = new ArrayList<FoodElement>(); для сбора неограниченого количества обрацов FoodElement
    — Внести в класс SplitTheBill метод для подстчета суммы и проверки суммы на предмет корректного деления на N персон с округлением до двух знаков.

--- src/main/java/FoodElement.java
+++ src/main/java/FoodElement.java
@@ -1,4 +1,5 @@
 public class FoodElement {
+<<<<<<< HEAD
 
     /*
     Калькулятор должен запоминать названия всех добавленных товаров, чтобы выводить все товары, которые были в него добавлены.
@@ -17,4 +18,13 @@ public class FoodElement {
     public String toString(){
         return "\n#"+counter+" — Название блюда: "+foodName+" — Цена в счёте: "+foodPrice+" р.";
     }
+=======
+    String foodName;
+    float foodPrice;
+
+    FoodElement(String foodName, float foodPrice) {
+        this.foodName = foodName;
+        this.foodPrice = foodPrice;
+    }
+>>>>>>> e149195 (ЧАСТЬ 2 // ПОКА НЕ РАБОТАЕТ КОРРЕКТНО // Добавил в Main.java перебор ввода с клавиатуры, чтобы собирать корректные названия и цены товаров.)
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
+// dev branch for Y.Practicum
 public class Main {
 
     public static void main(String[] args) {
+        /* 1. После запуска программа должна спрашивать у пользователя,
+        на скольких человек необходимо разделить счёт.*/
+        System.out.println("Привет, на скольких людей мы хотим разделить счёт?");
+        SplitTheBill bill = new SplitTheBill();
+        bill.quantityQuestionaire();
+        System.out.println("Число пользователей "+bill.personsQuantity);
+
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+
     }
 }
