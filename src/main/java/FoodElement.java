0e16a13d07963b79ac32e364c282c0c3f0b29a7d
Aleksandr Polyakov <master@artwist.ru>
2022-12-24 12:18:28 +0300

    ЧАСТЬ 2 // ПОКА НЕ РАБОТАЕТ КОРРЕКТНО // Добавил в Main.java перебор ввода с клавиатуры, чтобы собирать корректные названия и цены товаров.
    Требуется:
    — Внести в класс SplitTheBill атрибут вида List<FoodElement> billElementList = new ArrayList<FoodElement>(); для сбора неограниченого количества обрацов FoodElement
    — Внести в класс SplitTheBill метод для подстчета суммы и проверки суммы на предмет корректного деления на N персон с округлением до двух знаков.

---
+++ src/main/java/FoodElement.java
@@ -0,0 +1,9 @@
+public class FoodElement {
+    String foodName;
+    float foodPrice;
+
+    FoodElement(String foodName, float foodPrice) {
+        this.foodName = foodName;
+        this.foodPrice = foodPrice;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,9 +9,64 @@ public class Main {
         SplitTheBill bill = new SplitTheBill();
         bill.quantityQuestionaire();
         System.out.println("Число пользователей "+bill.personsQuantity);
+        System.out.println("=============");
+        System.out.println("Начните вводить список позиций из счёта.\nДля прекращения введите <Завершить>");
+        System.out.println("=============");
+        int i = 0; // счётчик для количества добавленных позиций.
 
+        while (true) {
+            i++; //переводим счётчик на одну позицию в плюс
+            while (true) { // цикл ожидания ввода корректного названия товара
+                System.out.println("Позиция счёта #" + i);
+                System.out.println("Введите название позиции #"+i);
+                Scanner scanner = new Scanner(System.in);
+                String positionName;
+                float positionPrice;
+                positionName = scanner.next();
+                positionName = positionName.toLowerCase();
+                boolean stopped = false;
+                if (positionName.equals("завершить")) {
+                    break;
+                } else if (isFloat(positionName)) {
+                    System.out.println("Кажется вы ввели число вместо имени позиции");
+                    break;
+                }
+                while (true) { //цикл ожидания ввода корректной стоимости товара
+                    System.out.println("Введите стоимость позиции #" + i);
+                    String positionPriceStr;
+                    positionPriceStr = scanner.next();
+                    positionPriceStr = positionPriceStr.toLowerCase();
+                    if (positionPriceStr.equals("завершить")) {
+                        break;
+                        stopped = true
+                    } else if (!isFloat(positionPriceStr)) {
+                        System.out.println("Кажется вы ввели НЕ число, попробуйте еще раз");
+                        break;
+                    } else {
+                        positionPrice = Float.parseFloat(positionPriceStr);
+                    }
+                }
+                if (stopped) {
+                    break;
+                }
+            }
+        }
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
 
     }
+
+    public static boolean isFloat(String str) {
+        try {
+            Float.parseFloat(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
 }
+
+
+
+
