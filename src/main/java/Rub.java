73068b98f7806693e62ab36190ccccf41de8c3cf
Alex32163216 <121754333+Alex32163216@users.noreply.github.com>
2023-01-28 10:30:38 +0300

    Проектная работа № 1 Скорректированы ошибки с окончанием слово рубль, добавлена информация, что "После ввода команды "завершить", ввод товаров прекратится.".

--- src/main/java/Rub.java
+++ src/main/java/Rub.java
@@ -4,11 +4,26 @@ public class Rub {
         float amountForPersons;
         String templatePut = "Итого, с каждого человека по %.2f";
         amountForPersons = totalAmount / amountPersons;
-        switch ((int)amountForPersons % 10){
+        switch ((int)amountForPersons % 100){
             default:{
                 System.out.println(String.format(templatePut, amountForPersons) + " рублей");
                 break;
             } case (1) : {
+                System.out.println(String.format(templatePut, amountForPersons) + " рубль");
+                break;
+            }
+            case (11) : {
+                System.out.println(String.format(templatePut, amountForPersons) + " рублей");
+                break;
+            }
+            case (21) :
+            case (31) :
+            case (41) :
+            case (51) :
+            case (61) :
+            case (71) :
+            case (81) :
+            case (91) : {
                 System.out.println(String.format(templatePut, amountForPersons) + " рубль");
                 break;
             }
