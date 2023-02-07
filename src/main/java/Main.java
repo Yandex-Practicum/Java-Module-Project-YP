e4d475bc4682a904117c70c5be2233c7497cd725
NickMikhailov <124362971+NickMikhailov@users.noreply.github.com>
2023-02-07 10:25:58 +0300

    возврат к исходному коду

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,9 @@
-import java.util.Scanner;
-
 // dev branch for Y.Practicum
 public class Main {
 
     public static void main(String[] args) {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        // Часть 1 - вводные параметры для счетчика (количество гостей)
-        int guestCount;
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("\nНа скольких человек необходимо разделить счет?");
-            while (true) {
-                if (scanner.hasNextInt()) {
-                    guestCount = scanner.nextInt();
-                    break;
-                }
-                else {
-                    scanner.next();
-                    System.out.println("Некорректный ввод, попробуйте еще раз");
-                }
-            }
-            if (guestCount>1) break;
-            else if (guestCount == 1) {
-                System.out.println("Если вы один, то вам за все и платить!");
-            }
-            else {
-                System.out.println("Некорректный ввод, попробуйте еще раз");
-            }
+        System.out.println("Привет Мир");
         }
-//        scanner.close();
-
-        // Часть 2 - создание чека и добавление товаров (в конструкторе)
-        Check check = new Check();
-
-        // Часть 3 - вывод результатов
-        check.print();
-
-        double checkPerOne = check.getSum()/guestCount;
-        Formatter rub = new Formatter();
-        System.out.println("Вас было "+guestCount+", значит с каждого по " + String.format("%.2f", checkPerOne) + rub.format(checkPerOne));
-    }
 }
