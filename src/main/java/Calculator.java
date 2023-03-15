aa890d125babd564fe3f85e340e4c2b89ae04d33
s-buvaka <s-buvaka@yandex-team.ru>
2023-03-15 12:01:42 +0700

    Add dev branch

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,28 @@
+public class Calculator {
+    public String GetRubleAddition(double num) {
+
+        int preLastDigit = (int) ((num % 100)/10);
+            if (preLastDigit == 1)
+            {
+                return "рублей";
+            } else {
+                switch ((int) (num % 10)) {
+                    case 1:
+                        return "рублю";
+                    case 2:
+                    case 3:
+                    case 4:
+                        return "рубля";
+                    default:
+                        return "рублей";
+                }
+            }
+        }
+        void calculate(double totalAmount, int numberOfPersons) {
+        double payBill = totalAmount / numberOfPersons;
+        String SumForOnePerson = "Каждый должен оплатить по:  %.2f";
+        System.out.println(String.format(SumForOnePerson, payBill) + " " + GetRubleAddition(payBill));
+    }
+}
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,53 @@
+import java.util.Scanner;
+import java.util.function.Function;
+
 // dev branch for Y.Practicum
 public class Main {
 
+
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        Calculator calc = new Calculator(); {
+        Scanner scanner = new Scanner(System.in)
+        String nameOfProduct = "";
+        double priceOfProduct = 0.0;
+        String fullList = "";
+        double totalAmount = 0.0;
+        while (true) { // не стал делать проверку по введенному значению. Она есть дальше при делении счета.
+            System.out.println("Введите название и цену товара! Если хотите закончить, то введите 'завершить'!");
+            nameOfProduct = scanner.next();
+                if (nameOfProduct.equalsIgnoreCase("завершить")) {
+                    System.out.println(fullList);
+                    String fullListsCost = "Общая сумма всего списка:  %.2f ";
+                    System.out.println(String.format(fullListsCost, totalAmount) + calc.GetRubleAddition(priceOfProduct));
+                    break;
+                } else {
+                    priceOfProduct = scanner.nextDouble();
+                    totalAmount = totalAmount + priceOfProduct;
+                    String cost = "%.2f";
+                    System.out.println("Товар добавлен!");
+                    fullList = (fullList.concat(nameOfProduct + " " + String.format(cost, priceOfProduct)) + "\n");
+                }
+        }
+        System.out.println("На скольких человек необходимо разделить счет?");
+            int numberOfPersons = 0;
+            while (true) {
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Не является числом, повторите ввод!");
+                    String numberOfPersons1 = scanner.next();
+                }
+                while (scanner.hasNextInt()) {
+                    numberOfPersons = scanner.nextInt();
+                    if (numberOfPersons <= 1) {
+                        System.out.println("Количество персон должно быть больше 1. Повторите ввод!");
+                    } else if (numberOfPersons > 1) {
+                        System.out.println("Разделим сумму счета на " + numberOfPersons + "!");
+                        calc.calculate(totalAmount, numberOfPersons);
+                        return;
+                    }
+                    break;
+                }
+            }
+        }
     }
 }
+
