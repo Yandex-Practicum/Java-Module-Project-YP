6cd547a703a3242cb834200219c751079b08fb4a
V3hby <V3hby@yandex.ru>
2023-03-16 08:13:59 +0700

    Add dev branch ver2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,59 +6,58 @@ public class Main {
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        {
-            Scanner scanner = new Scanner(System.in);
-            double priceOfProduct = 0.0;
-            String nameOfProduct = "";
-            String fullList = "";
-            double totalAmount = 0.0;
-            String cost = "%.2f";
-            while (true) {
-                System.out.println("Введите название и цену товара! Если хотите закончить, то введите 'завершить'!");
-                nameOfProduct = scanner.nextLine();
-                if (nameOfProduct.equalsIgnoreCase("завершить")) {
-                    System.out.println(fullList);
-                    String fullListsCost = "Общая сумма всего списка:  %.2f ";
-                    System.out.println(String.format(fullListsCost, totalAmount) + calc.getRubleAddition(priceOfProduct));
-                    break;
-                } else {
-                    while (true) {
-                        while (!scanner.hasNextDouble()) {
-                            System.out.println("Не является числом, повторите ввод!");
-                            scanner.nextLine();
-                        }
-                        priceOfProduct = scanner.nextDouble();
+        Scanner scanner = new Scanner(System.in);
+        double priceOfProduct = 0.0;
+        String nameOfProduct = "";
+        String fullList = "";
+        double totalAmount = 0.0;
+        String cost = "%.2f";
+        while (true) {
+            System.out.println("Введите название и цену товара! Если хотите закончить, то введите 'завершить'!");
+            nameOfProduct = scanner.nextLine();
+            if (nameOfProduct.equalsIgnoreCase("завершить")) {
+                System.out.println(fullList);
+                String fullListsCost = "Общая сумма всего списка:  %.2f ";
+                System.out.println(String.format(fullListsCost, totalAmount) + calc.getRubleAddition(priceOfProduct));
+                break;
+            } else {
+                while (true) {
+                    while (!scanner.hasNextDouble()) {
+                        System.out.println("Не является числом, повторите ввод!");
                         scanner.nextLine();
-                        if (priceOfProduct <= 0) {
-                            System.out.println("Цена должны быть больше 0! Повторите ввод!");
-                        } else {
-                            totalAmount = totalAmount + priceOfProduct;
-                            System.out.println("Товар добавлен!");
-                            fullList = (fullList.concat(nameOfProduct + " " + String.format(cost, priceOfProduct)) + "\n");
-                            break;
-                        }
+                    }
+                    priceOfProduct = scanner.nextDouble();
+                    scanner.nextLine();
+                    if (priceOfProduct <= 0) {
+                        System.out.println("Цена должны быть больше 0! Повторите ввод!");
+                    } else {
+                        totalAmount = totalAmount + priceOfProduct;
+                        System.out.println("Товар добавлен!");
+                        fullList = (fullList.concat(nameOfProduct + " " + String.format(cost, priceOfProduct)) + "\n");
+                        break;
                     }
                 }
             }
-            System.out.println("На скольких человек необходимо разделить счет?");
-            int numberOfPersons = 0;
-            while (true) {
-                while (!scanner.hasNextInt()) {
-                    System.out.println("Не является числом, повторите ввод!");
-                    scanner.next();
-                }
-                numberOfPersons = scanner.nextInt();
-                if (numberOfPersons <= 1) {
-                    System.out.println("Количество персон должно быть больше 1. Повторите ввод!");
-                } else {
-                    System.out.println("Разделим сумму счета на " + numberOfPersons + "!");
-                    calc.calculate(totalAmount, numberOfPersons);
-                    return;
-                }
+        }
+        System.out.println("На скольких человек необходимо разделить счет?");
+        int numberOfPersons = 0;
+        while (true) {
+            while (!scanner.hasNextInt()) {
+                System.out.println("Не является числом, повторите ввод!");
+                scanner.next();
+            }
+            numberOfPersons = scanner.nextInt();
+            if (numberOfPersons <= 1) {
+                System.out.println("Количество персон должно быть больше 1. Повторите ввод!");
+            } else {
+                System.out.println("Разделим сумму счета на " + numberOfPersons + "!");
+                calc.calculate(totalAmount, numberOfPersons);
+                return;
             }
         }
     }
 }
+
 
 
 
