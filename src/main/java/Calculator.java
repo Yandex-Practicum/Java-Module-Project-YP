3748fe8d520009db06a2f7adad5252cf060a1984
V3hby <V3hby@yandex.ru>
2023-03-16 08:14:51 +0700

    Add changes dev branch ver2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,24 +1,24 @@
 public class Calculator {
     public String getRubleAddition(double num) {
 
-        int preLastDigit = (int) ((num % 100)/10);
-            if (preLastDigit == 1)
-            {
-                return "рублей";
-            } else {
-                switch ((int) (num % 10)) {
-                    case 1:
-                        return "рублю";
-                    case 2:
-                    case 3:
-                    case 4:
-                        return "рубля";
-                    default:
-                        return "рублей";
-                }
+        int preLastDigit = (int) ((num % 100) / 10);
+        if (preLastDigit == 1) {
+            return "рублей";
+        } else {
+            switch ((int) (num % 10)) {
+                case 1:
+                    return "рублю";
+                case 2:
+                case 3:
+                case 4:
+                    return "рубля";
+                default:
+                    return "рублей";
             }
         }
-        void calculate(double totalAmount, int numberOfPersons) {
+    }
+
+    void calculate(double totalAmount, int numberOfPersons) {
         double payBill = totalAmount / numberOfPersons;
         String SumForOnePerson = "Каждый должен оплатить по:  %.2f";
         System.out.println(String.format(SumForOnePerson, payBill) + " " + getRubleAddition(payBill));
