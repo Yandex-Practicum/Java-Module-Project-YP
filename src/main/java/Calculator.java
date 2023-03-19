3bbf786ca11cd088c841ec6506bd18b88bc79e26
Paffnutiy <paffnutiy@gmail.com>
2023-03-19 09:58:32 +0300

    Fist steps draft

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class Calculator {
+    String itemNameList = "";
+    int numItems;
+    float price;
+
+
+    Calculator (String itemNameList, float price, int numItems ) {
+        this.itemNameList = itemNameList;
+        this.price = price;
+        this.numItems = numItems;
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,46 @@
 // dev branch for Y.Practicum
+
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Привет мир!!!");
+        System.out.println("На сколько человек желаете разделить счёт?");
+        int numPerson = scanner.nextInt();
+
+        while (numPerson <= 1) {
+            System.out.println("Значение не подходит, введите значение больше 1");
+            numPerson = scanner.nextInt();
+        }
+
+        Calculator calcPersonBill = new Calculator("", 0, 0);
+
+        while (true) {
+            System.out.println("Введите Название товара");
+// Новый элемент добавляется в указанную позицию (в данном случае — в начало)
+            //arrayList.add(0, "Watson");
+            calcPersonBill.itemNameList = scanner.nextLine();
+            System.out.println(calcPersonBill.itemNameList);
+
+            System.out.println("Введите цену товара");
+            calcPersonBill.price =+ scanner.nextFloat();
+
+            System.out.println("Товар добавлен");
+            System.out.println("Еще добавим?");
+            //String keyWord = scanner.nextLine();
+            if  ((scanner.nextLine()).equalsIgnoreCase("Завершить")) {
+                break;
+            } else calcPersonBill.numItems++;
+        }
+        System.out.println(calcPersonBill.itemNameList);
+       System.out.println(calcPersonBill.price);
+
+        }
+
     }
-}
+
