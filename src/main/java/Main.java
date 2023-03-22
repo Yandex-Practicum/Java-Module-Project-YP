3ba90c1de08aa28f92f5e7a42dd110180bab2195
Alex <kailarstern@mail.ru>
2023-03-22 08:37:39 +0300

    Долгожданный коммит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int guests= 0;
+        int guests = 0;
         boolean guestsNum = false;
         while (!guestsNum) {
             System.out.print("Введите количество гостей: ");
-            guests = scanner.nextInt();
-            if (guests <= 1) {
-                System.out.println("Некорректное значение. Количество гостей должно быть больше 1.");
+            if (scanner.hasNextInt()) {
+                guests = scanner.nextInt();
+                if (guests <= 1) {
+                    System.out.println("Некорректное значение. Количество гостей должно быть больше 1.");
+                } else {
+                    guestsNum = true;
+                }
             } else {
-                guestsNum = true;
+                System.out.println("Введите корректное число!");
+                scanner.next();
             }
         }
 
@@ -23,12 +28,24 @@ public class Main {
             System.out.print("Введите название товара: ");
             String itemName = scanner.next();
 
-            System.out.print("Введите стоимость товара (в формате рубли.копейки): ");
-            double itemCost = scanner.nextDouble();
-
-            totalValue += itemCost;
-            itemList += itemName + " - " + itemCost + " руб." + "\n";
-            System.out.println("Товар " + itemName + " успешно добавлен.");
+            boolean validCost = false;
+            while (!validCost) {
+                System.out.print("Введите стоимость товара (в формате рубли.копейки): ");
+                if (scanner.hasNextDouble()) {
+                    double itemCost = scanner.nextDouble();
+                    if (itemCost >= 0) {
+                        totalValue += itemCost;
+                        itemList += itemName + " - " + String.format("%.2f", itemCost) + " руб." + "\n";
+                        System.out.println("Товар " + itemName + " успешно добавлен.");
+                        validCost = true;
+                    } else {
+                        System.out.println("Стоимость товара не может быть отрицательной.");
+                    }
+                } else {
+                    System.out.println("Введите корректное число!");
+                    scanner.next();
+                }
+            }
 
             System.out.print("Хотите добавить ещё один товар? (Введите \"Завершить\", чтобы закончить): ");
             String userChoice = scanner.next();
@@ -37,21 +54,30 @@ public class Main {
             }
         }
 
-        System.out.println("Добавленные товары:");
+        System.out.println("Добавленные товары: ");
         System.out.println(itemList);
 
         double costPerPerson = totalValue / guests;
         String formattedCost = String.format("%.2f", costPerPerson);
         int rubles = (int) costPerPerson;
         String rubleSuffix;
-        if (rubles == 1) {
+
+        int lastDigit = rubles % 10;
+        int lastTwoDigits = rubles % 100;
+
+        if (lastTwoDigits >= 11 && lastTwoDigits <= 19) {
+            rubleSuffix = "рублей";
+        } else if (lastDigit == 1) {
             rubleSuffix = "рубль";
-        } else if (rubles >= 2 && rubles <= 4) {
+        } else if (lastDigit >= 2 && lastDigit <= 4) {
             rubleSuffix = "рубля";
         } else {
             rubleSuffix = "рублей";
         }
+
         System.out.println("Каждый гость должен заплатить " + formattedCost + " " + rubleSuffix + ".");
     }
 }
+
+
 
