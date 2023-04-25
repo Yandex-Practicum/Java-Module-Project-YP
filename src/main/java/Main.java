91e46d84047770c9766611a0d62b17ff92833c7d
Dmitry-664 <Rogachev-64@mail.ru>
2023-04-25 14:47:46 +0300

    Консольное приложение №1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,41 +14,6 @@ public class Main {
             }
         }
 
-//        Korzina korzina = new Korzina();
-//        while (true) {
-//            System.out.println("Вы хотите добавить товар ?");
-//            printMenu();
-//            int command = scanner.nextInt();
-//            if ( command == 1 ) {
-//                System.out.println("Введите название товара и его стоимость");
-//                String product = scanner.nextLine();
-//                String[] words = product.split(" ");
-//                String name = words[0];
-//                double price = Double.parseDouble(words[1]);
-//
-//            } else if ( command == 2 ) {
-//                System.out.println("Завершение программы");
-//            }
-//        }
-//
-//
-//        System.out.println("Введённое число " + peopleCount);
-//    }
-//    public static void printMenu() {
-//        System.out.println("1 - Добавить товар");
-//        System.out.println("2 - Завершить программу");
-//
-//    }
-//    public static String addProduct(String productList, String product) {
-//        if (!product.isEmpty()) {
-//             productList + product + ".";
-//        }
-//        else {
-//            return productList + product + ", ";
-//        }
-//
-//    }
-
         double calculator = 0;
         double price = 0;
         String nameProduct = "";
@@ -101,75 +66,6 @@ public class Main {
                     break;
             }
             System.out.println("Каждому нужно заплатить " + String.format("%.2f", everyCheck) + " " + rub + ".");
-
-//        System.out.println("Названия добавленных товаров " + nameProduct);
-//        System.out.println("Товар успешно добавлен. Хотите ли Вы добавить ещё товар?");
-//            if ( nameProduct.equalsIgnoreCase (product)) {
-//                System.out.println("Итого");
-//                break;
-//            } esle
-//
-//        String product = Scanner.nextInt();
-//
-//        System.out.println("Введите стоимость продукта");
-//        double sum = Scanner.nextDouble();
-//        calculator = sum + calculator;
-//        System.out.println("Сумма продуктов " + calculator);
-//
-//    }
-//    public static void calculator() {
-//        double sum = 0;
-//        String product;
-//        double price;
-//        sum () {
-//            while ( sum >= 0 ) {
-//                System.out.println("Введите стоимость продукта");
-//                double price = Scanner.nextDouble();
-//                sum = sum + price;
-//                System.out.println("Сумма продуктов " + sum);
-//                break;
-//            }
-//        product () {
-//                while ( product )
-//            }
-//        }
-//        System.out.println("Введите стоимость продукта");
-//        double sum = Scanner.nextDouble();
-//        calculator = sum + calculator;
-//        System.out.println("Сумма продуктов " + calculator);
-//
-//
-//    }
-//    public static void calculator() {
-//        double sum = 0;
-//        String products;
-//        String product;
-//        String finish = "Завершить";
-//        double price = 0;
-//
-//        void name2() {
-//            System.out.println("Введите название товара ");
-//        while (true) {
-//            product = scanner.nextLine();
-//            if (product.equalsIgnoreCase(finish)) {
-//                System.out.println("Ввод завершён");
-//                break;
-//            } else {
-//                products = products + product;
-//                System.out.println("Товар успешно добавлен. Хотите ли Вы добавить ещё товар?");
-//                System.out.println("Названия добавленных товаров " + products);
-//            }
-//
-//            System.out.print("Введите стоимость товара: ");
-//            price = scanner.nextDouble();
-//            if (price <= 0) {
-//                System.out.println("Вы ввели неверные данные. Повторите ввод.");
-//            } else if (price > 0) {
-//                sum = sum + price;
-//                System.out.println("Общая сумма товаров " + sum + " рублей");
-//            }
-//        }
-//    }
 
         }
     }
