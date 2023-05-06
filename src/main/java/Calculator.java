6da691eb5924c5fdfb9a0d6ae1c10822d99c4497
AlexOtkidach <otkidach.lesha@yandex.ru>
2023-05-06 17:36:06 +0300

    Консольное приложение. Проектная работа №1.

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,44 @@
+public class Calculator {
+    private final int sumGuests;
+    private double totalPrice;
+    public String list = "";
+
+    public Calculator(int sumGuests) {
+
+        this.sumGuests = sumGuests;
+
+    }
+
+    public void add(Product p) {
+        totalPrice += p.price;
+
+        list += p.productName + "\n";
+    }
+public String getResult() {
+    var part = totalPrice / sumGuests;
+            //Чтобы округлить дробное число только до двух чисел после запятой, можно использовать строковый шаблон: String.format("%.2f", double);.
+    var r = String.format("%.2f",part);
+
+    //Чтобы поставить правильное окончание, вы можете округлить дробное значение и вывести слово "рубль" в правильном падеже. Для округления дробного числа в меньшую сторону можно использовать метод floor() класса Math.
+var rub = Math.floor(part);
+
+var suffix = getSuffix (rub);
+
+           return r + " рубл" + suffix;
+    }
+
+    private String getSuffix(double rub) {
+//        0,5 - 20 ей
+//        1:ь
+//        2,3,4я
+                if (rub == 1) {
+                    return "ь";
+                } else if (rub == 2 || rub == 3 || rub == 4){
+                    return "я";
+                } else {
+                    return "ей";
+                }
+
+    }
+
+}
