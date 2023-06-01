657e4ca2a0a05448e5c88c7c94abf8bea8ae9ee8
dernom <der_nom@mail.ru>
2023-06-01 18:25:47 +0300

    Первые строки Импорт сканера

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
+import java.util.Scanner; //ипортируем сканер
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет! На какое количество человек делим счет?");
         Scanner guest = new Scanner(System.in);
         while (true){
-            int x = guest.nexInt();
+            int x = guest.nextInt();
 
             if (x == 1){
                 System.out.println("Должно быть больше одного человека");
