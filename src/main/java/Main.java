24a227907e2c5458ef9e6ce0162776dfb46690e6
dernom <der_nom@mail.ru>
2023-06-01 19:28:24 +0300

    Первые строки Импорт сканера Создана новая ветка

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,16 +4,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет! На какое количество человек делим счет?");
         Scanner guest = new Scanner(System.in);
+
         while (true){
             int x = guest.nextInt();
-
-            if (x == 1){
-                System.out.println("Должно быть больше одного человека");
-                return;
-            } else if (x < 1){
-                System.out.println("Не может быть меньше одного человека");
-            } else
+            if (x > 1){
+                System.out.println("Вы ввели правильно");
                 break;
+            }
+                System.out.println("Должно быть больше одного человека");
+
         }
     }
-} //как запустить в терминале????
+}
