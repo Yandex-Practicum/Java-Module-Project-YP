cf89f4d70b9a714cc6b16b5d16da9873caf2a824
JollyRogerRu <satortunov@yandex.ru>
2023-06-02 14:40:32 +0300

    Результаты работы 02.06.2023 14-40

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,9 @@ public class Main extends programActions {
 
     public static void main(String[] args)
     {
-        roublesAndKopecks(326473.199999);
+        double out = 0;
+        String s = moneyOut(roublesAndKopecks(23543.186792));
+        System.out.println(s);
         ///peopleQuantity();
     }
 
--- src/main/java/helpLibrary.java
+++ src/main/java/helpLibrary.java
@@ -2,27 +2,70 @@ public class helpLibrary
 {
     public static String roublesAndKopecks (double amountSum)
     {
-        System.out.println(amountSum);
         String formatResult = Double.toString(amountSum);
         int indexDot = formatResult.indexOf(".");
-        System.out.println(indexDot);
-
-        String roubles = formatResult.substring(indexDot - 2, indexDot);
-        System.out.println(roubles);
-        String kopecks = formatResult.substring(indexDot+1, indexDot + 3);
-        System.out.println(kopecks);
-       /* if (formatResult.length() < 5)
-        {
-            formatResult = "0" + formatResult;
-        }
-        else if(formatResult.length() > 5)
-        {
-            formatResult = formatResult.substring(formatResult.length()-5, formatResult.length());
-        }
-        String roubles = formatResult.substring(0, 2);
-        String kopecks = formatResult.substring(3, 5);
-        System.out.println(formatResult);
-        System.out.println(roubles + " " + kopecks); */
+        formatResult = formatResult.substring(indexDot - 2, indexDot + 3);;
         return formatResult;
     }
-}
+
+
+    public static String moneyOut(String moneyString)
+    {
+        String roublesStr = moneyString.substring(0, 2);
+        String kopecksStr = moneyString.substring(3, 5);
+        int roubles = Integer.parseInt(roublesStr);
+        String outRoubles = "Ру";
+
+            if (roubles >= 11 && roubles < 20)
+            {
+               outRoubles += "блей";
+               return(outRoubles);
+            }
+            else
+            {
+                roublesStr = roublesStr.substring(1, 2);
+                roubles = Integer.parseInt(roublesStr);
+                switch(roubles)
+                {
+                    case 1:
+                      outRoubles += "бль";
+                      break;
+                    case 2:
+                        outRoubles += "бля";
+                        break;
+                    case 3:
+                        outRoubles += "бля";
+                        break;
+                    case 4:
+                        outRoubles += "бля";
+                        break;
+                    case 5:
+                        outRoubles += "блей";
+                        break;
+                    case 6:
+                        outRoubles += "блей";
+                        break;
+                    case 7:
+                        outRoubles += "блей";
+                        break;
+                    case 8:
+                        outRoubles += "блей";
+                        break;
+                    case 9:
+                        outRoubles += "блей";
+                        break;
+
+                }
+
+                return (outRoubles);
+            }
+        }
+
+
+
+
+
+    }
+
+
+
