6d465222fa16d11abb257d451c10b1ba216506d6
Sssenju <iiimmmortal05@mail.ri>
2023-07-04 09:06:21 +1100

    Последняя надежда моих безнадежных попыток стать программистом, Домашняя работа номер 1 спринт 2 .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,66 +1,94 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
+        System.out.println("Введите число гостей (от 2 человек):");
 
-                int guests;
-                while (true) {
-                    System.out.println("Введите число гостей (от 2-х человек): ");
-                    guests = Integer.parseInt(myObj.nextLine());
-                    if (guests <= 1) {
-                        System.out.println("Некорректное число гостей. Попробуйте еще раз.");
-                    } else {
-                        break;
-                    }
-                }
-                System.out.println("Введенное количество гостей: " + guests);
-
-                double goods = 0;
-                String userInput;
-
-                do {
-                    System.out.println("Введите название товара: ");
-                    String itemName = myObj.nextLine();
-
-                    System.out.println("Введите стоимость товара: ");
-                    double itemPrice;
-                    while (true) {
-                        if (myObj.hasNextDouble()) {
-                            itemPrice = myObj.nextDouble();
-                            if (itemPrice <= 0) {
-                                System.out.println("Неверная цена товара. Попробуйте еще раз.");
-                            } else {
-                                break;
-                            }
-                        } else {
-                            System.out.println("Некорректный ввод. Попробуйте еще раз.");
-                            myObj.nextLine();
-                        }
-                    }
-
-                    goods += itemPrice;
-
-                    System.out.println("Добавлен товар " + itemName + ", " + itemPrice);
-                    System.out.println("Хотите ли добавить еще товар? (Введите 'завершить' для окончания): ");
-                    myObj.nextLine();
-                    userInput = myObj.nextLine().toLowerCase();
-
-                } while (!userInput.equals("завершить"));
-
-                double result = goods / guests;
-                System.out.printf("%.2f %s", result, rubleEnding(result));
-            }
-
-            public static String rubleEnding(double amount) {
-                int rubles = (int) amount;
-
-                if (rubles % 10 == 1 && rubles % 100 != 11) {
-                    return "рубль";
-                } else if (rubles % 10 >= 2 && rubles % 10 <= 4 && (rubles % 100 < 10 || rubles % 100 >= 20)) {
-                    return "рубля";
-                } else {
-                    return "рублей";
-                }
-            }
+        int guests = 0;
+        try {
+            guests = myObj.nextInt();
+            myObj.nextLine(); // Очистка буфера после чтения числа
+        } catch (NumberFormatException e) {
+            System.out.println("Некорректное число гостей");
+            return;
         }
+
+        if (guests <= 1) {
+            System.out.println("Некорректное число гостей");
+            return;
+        }
+
+        System.out.println("Введенное количество гостей: " + guests);
+        double totalGoods = 0;
+        List<Item> items = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Введите название товара:");
+            String itemName = myObj.nextLine();
+
+            if (itemName.equalsIgnoreCase("завершить")) {
+                break;
+            }
+
+            System.out.println("Введите стоимость товара:");
+            double itemPrice = 0;
+            try {
+                itemPrice = Double.parseDouble(myObj.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Неверная цена товара");
+                return;
+            }
+
+            if (itemPrice <= 0) {
+                System.out.println("Неверная цена товара");
+                return;
+            }
+
+            Item item = new Item(itemName, itemPrice);
+            items.add(item);
+            totalGoods += itemPrice;
+            System.out.println("Добавлен товар: " + itemName + ", Цена: " + itemPrice);
+            System.out.println("Хотите ли добавить еще товар? (Введите 'Завершить', чтобы закончить)");
+        }
+
+        System.out.println("Список товаров:");
+        for (Item item : items) {
+            System.out.println(item.getName() + ", Цена: " + item.getPrice());
+        }
+
+        double result = totalGoods / guests;
+        System.out.printf("Сумма на человека: %.2f %s", result, rubleEnding(result));
+    }
+
+    public static String rubleEnding(double amount) {
+        int rubles = (int) amount;
+        if (rubles % 10 == 1 && rubles % 100 != 11) {
+            return "рубль";
+        } else if (rubles % 10 >= 2 && rubles % 10 <= 4 && (rubles % 100 < 10 || rubles % 100 >= 20)) {
+            return "рубля";
+        } else {
+            return "рублей";
+        }
+    }
+}
+
+class Item {
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
