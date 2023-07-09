1ccae54d576e42d1c10ef2a88462f7559f04ff17
VikiLeo <viktoria_leon@mail.ru>
2023-07-09 17:41:07 +0300

    итоговый вариант 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
 
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("На сколько человек необходимо разделить счет?");
+                System.out.println("На сколько человек необходимо разделить счет?");
         Product foodBasket = new Product();
         foodBasket.basicProgram();
     }
--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -11,51 +11,76 @@ public class Product {
     Scanner scanner=new Scanner(System.in);
     void basicProgram() {
         while (true) {
-            numberOfGuests = scanner.nextInt();
-            if (numberOfGuests > 1){
-                groceryList();
-                break;
+                        if (scanner.hasNextInt()) {
+                                                        numberOfGuests = Integer.parseInt(scanner.nextLine());
+
+                if (numberOfGuests > 1){
+                    showGroceryList();
+                    break;
+                } else {
+                    System.out.println("Введено некорректное значение, попробуйте еще раз.");
+                }
             } else {
-                System.out.println("Введено некорректное значение, попробуйте еще раз.");
-            }
-        }
+                            System.out.println("Введено некорректное значение, попробуйте еще пару раз.");
+                            scanner.nextLine();
+                                                }
     }
-    void groceryList() {
+    }
+    void showGroceryList() {
         while (true) {
             System.out.println("Введите название товара");
-            productName = scanner.next();
+                        productName = scanner.next();
             orderList = orderList + productName + "\n";
             System.out.println("Введите стоимость товара в формате:рубли.копейки.");
-            price = scanner.nextDouble();
+            inputOfGoodsCost();
             totalAmount = totalAmount + price;
-            System.out.println("Товар успешно добавлен. Хотите ли вы добавить еще один товар?" + "\n"+ "Если да - введите любое слово или букву." +"\n"+ "Если нет - введите команду: завершить");
+            System.out.println("Товар успешно добавлен. Хотите ли вы добавить еще один товар?\n" + "Если да - введите любое слово или букву.\n" + "Если нет - введите команду: завершить");
             command = scanner.next();
             String complete = "завершить";
             if (command.equalsIgnoreCase(complete)) {
                 System.out.println("Добавленные товары:" +"\n" + orderList);
                 finalScore = totalAmount/numberOfGuests;
-                int numberRoundUp = (int) Math.floor(finalScore);
-
-                switch (numberRoundUp%100) {
-                    case 11,12,13,14:
-                        rubles = "рублей";
-                        break;
-                    default:
-                        switch (numberRoundUp%10) {
-                            case 1:
-                                rubles = "рубль";
-                                break;
-                            case 2,3,4:
-                                rubles = "рубля";;
-                                break;
-                            case 5,6,7,8,9,0:
-                                rubles = "рублей";
-                                break;
-                        }
-                }
+                performFormatting();
                 System.out.println("Каждый человек должен заплатить - " + String.format("%.2f", finalScore) +" " + rubles+".");
                 break;
             }
         }
     }
+    void inputOfGoodsCost () {
+        while (true) {
+            if (scanner.hasNextDouble()) {
+                price = scanner.nextDouble();
+                if (price > 0){
+                    break;
+                } else {
+                    System.out.println("Введено некорректная стоимость товара, попробуйте еще 1 раз.");
+                }
+            } else {
+                System.out.println("Введено некорректная стоимость товара, попробуйте еще раз.");
+                scanner.next();
+            }
+        }
+    }
+    void performFormatting () {
+        int numberRoundUp = (int) Math.floor(finalScore);
+
+        switch (numberRoundUp%100) {
+            case 11,12,13,14:
+                rubles = "рублей";
+                break;
+            default:
+                switch (numberRoundUp%10) {
+                    case 1:
+                        rubles = "рубль";
+                        break;
+                    case 2,3,4:
+                        rubles = "рубля";;
+                        break;
+                    case 5,6,7,8,9,0:
+                        rubles = "рублей";
+                        break;
+                }
+        }
+    }
 }
+
