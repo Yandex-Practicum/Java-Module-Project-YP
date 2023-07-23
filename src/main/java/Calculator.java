99ef39a4dfd2f227fee7f757569c0b66f6b0470a
sysooevAndrey <sysoyev.and@gmail.com>
2023-07-24 01:52:59 +0300

    2 commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -24,8 +24,6 @@ public class Calculator {
         if (costPerPeople < 2) endString = "бль";
         else if (costPerPeople > 2 && costPerPeople < 5) endString = "бля";
         else endString = "блей";
-//        System.out.println("Добавленные товары:" + allProduct + "\nКаждый человек должен заплатить " +
-//                +costPerPeople + " ру" + endString + ".");
         String finalOutputTemplate = "Добавленные товары:%s\nКаждый человек должен заплатить %.2f ру%s.";
         String finalOutput = String.format(finalOutputTemplate,allProduct,costPerPeople,endString);
         System.out.println(finalOutput);
