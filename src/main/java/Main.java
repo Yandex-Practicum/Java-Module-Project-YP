451a35c768d9afa4c0c10eb93aa7d1c358ae5729
sysooevAndrey <sysoyev.and@gmail.com>
2023-07-24 01:47:12 +0300

    1 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,54 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Укажите количество человек, на которых необходимо разделить счёт:");
+
+        //Ввод количества людей и проверка
+        int peopleAmount;
+        while (true) {
+            peopleAmount = scanner.nextInt();
+            if (peopleAmount == 1 || peopleAmount == 0) {
+                System.out.println("Пожалуйста, укажите большее количество человек:");
+            } else if (peopleAmount <= -1) {
+                System.out.println("Указано отрицательное число. Пожалуйста, введите другое число:");
+            } else {
+                break;
+            }
+        }
+
+        //Добавление товаров в корзину
+
+        Calculator calculator = new Calculator();
+
+        System.out.println("Добавьте товар в корзину. Для этого укажите его наименование:");
+
+        while (true) {
+            String productName = scanner.next();
+            if (productName.equalsIgnoreCase("завершить")) {
+                break;
+            } else {
+                System.out.println("Укажите стоимость товара:");
+                while (true) {
+                    float price = scanner.nextFloat();
+                    if (price > 0) {
+                        calculator.addNameToCart(productName);
+                        calculator.costing(price);
+                        System.out.println("Введите название следующего товара, чтобы добавить его в корзину. " +
+                                "Чтобы завершить добавление товаров и расчитать стоимость для каждого человека введите \"Завершить\":");
+                        break;
+                    }
+                    else {
+                        System.out.println("Введено отрицательное число. Пожалуйста, повторно введите стоимость:");
+                    }
+
+                }
+            }
+        }
+        calculator.calculatePerPeople(peopleAmount);
+        calculator.finalOutput();
     }
 }
