ff9a2f21748b7111f18268083ffc4d91baef01f9
Andrey49311 <AndreyAlksv@yandex.ru>
2023-08-30 01:52:51 +0300

    Проект Спринт 2.

---
+++ src/main/java/Good.java
@@ -0,0 +1,18 @@
+public class Good {
+    private String name;
+    private Double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Good(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
