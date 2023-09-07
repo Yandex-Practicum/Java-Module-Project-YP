fe5d897ab735b6ba4d5432811b17be08247f541f
ismateush <ismateush@gmail.com>
2023-09-07 23:36:06 +0300

    Добавила валидацию

--- src/main/java/People.java
+++ src/main/java/People.java
@@ -6,6 +6,10 @@ public class People {
         System.out.println("Сколько человек желает разделить счёт?");
         int people = 0;
         while (people <= 1) {
+            while(!scanner.hasNextInt()) {
+                System.out.println("Некорректный ввод. Ожидается целое число");
+                scanner.next();
+            }
             people = scanner.nextInt();
             if (people < 1) {
                 System.out.println("Вы ввели некорректное значение. Попробуйте ввести еще раз");
--- src/main/java/Сalculator.java
+++ src/main/java/Сalculator.java
@@ -4,7 +4,6 @@ import java.util.Locale;
 
 public class Сalculator {
     double addProducts() {
-
         String productsList = "Добавленные товары:\n";
         double totalAmount = 0;
         double productPrice;
@@ -12,8 +11,13 @@ public class Сalculator {
             Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
             System.out.println("Введите название товара");
             String productName = scanner.nextLine();
-            System.out.println("Введите стоимость товара");
+            System.out.println("Введите стоимость товара в формате рубли.копейки");
+            while(!scanner.hasNextDouble()) {
+                System.out.println("Некорректный ввод. Введите стоимость товара в формате рубли.копейки");
+                scanner.next();
+            }
             productPrice = scanner.nextDouble();
+
             System.out.println(productName + " у вас в корзине!");
             totalAmount = totalAmount + productPrice;
             productsList = productsList.concat(productName).concat("\n");
