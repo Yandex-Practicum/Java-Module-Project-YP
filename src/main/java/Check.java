e71f259f06ab1464cba00b286a10bf1a98eaf1d1
Kovalev Timofey <timkov1506@gmail.com>
2023-09-17 00:20:13 +0300

    ошибка в окончании рубль

--- src/main/java/Check.java
+++ src/main/java/Check.java
@@ -11,17 +11,20 @@ public class Check {
         boolean point = true;
         float sum = 0.00f;
         String item;
-
-        while (point) {// Цикл на ввод товара и подсчет суммы чека
+        while (point){// Цикл на ввод товара и подсчет суммы чека
             System.out.println("Введите товар\n Если хотите завершить, веведите 'Завершить'.");
             item = scanner.nextLine();
             if (input.StopOrNot(item))//проверка на завершение ввода
-                break;
+                point = false;
             if (getPrice(item)== 0.00f)//проверка на правильность ввода цены
-                break;
-            sum += getPrice(item);//сумма чека
-            check.append("\n" + item + " " +format.ruble(getPrice(item)));
+                System.out.println("Введите товар, стоимость которого больше нуля");
+            else {
+                sum += getPrice(item);//сумма чека
+                check.append("\n" + item + " " +format.ruble(getPrice(item)));
             }
+        }
+
+
 
         System.out.println("Добавленные товары:" + check);
         return sum;
--- src/main/java/Format.java
+++ src/main/java/Format.java
@@ -5,10 +5,17 @@ public class Format {
         float result = sum / Float.parseFloat(String.valueOf(people));
         return result;
     }
-    public String ruble(float price){
-        price = price * 100 %10;
-        if (price == 0) return "рубля";
-        else return "рубль";
+    public String ruble(float floatPrice){
+        int price = (int) (floatPrice * 100);
+        price = price /100 % 100;
+        if (price > 20)
+            price %= 10;
+        else if (price < 5) return "рублей";
+        else price %= 10;
+
+        if (price == 1)return "рубль";
+        else if (price < 5)  return "рубля";
+        else return "рублей";
     }
     public String endString(float cash) {
         String endLine = String.format("Каждый должен оплатить  %.2f %s", cash, ruble(cash));
