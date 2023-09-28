70460df3f26b479a18ac699c9c7726e61b727ac6
Dzhekell <revan120@bk.ru>
2023-09-28 16:29:44 +0300

    2 версия

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,38 +2,47 @@ import java.util.Scanner;
 public class Calculator {
     Scanner scanner = new Scanner(System.in);
     double sum;
-    String goodsList = "";
+    String goods = "";
     String currency;
 
     void addGoods() {
         while (true) {
             System.out.println("Введите название товара: ");
             String addProduct = scanner.next();
-            goodsList = goodsList + "\n" + addProduct;
+            goods = goods + "\n" + addProduct;
 
             System.out.println("Введите цену товара:");
-            double productPrice = scanner.nextDouble();
-            sum += productPrice;
-
-            System.out.println("Товар успешно добавлен!");
-            System.out.println("Хотите добавить еще один товар? Если нет, введите \"Завершить\"");
-            String answer = scanner.next();
-            if (answer.equalsIgnoreCase("Завершить")) {
-                break;
+            if (scanner.hasNextDouble()) {
+                double productPrice = scanner.nextDouble();
+                if (productPrice > 0) {
+                    sum += productPrice;
+                    System.out.println("Товар успешно добавлен!");
+                    System.out.println("Хотите добавить еще один товар? Если нет, введите \"Завершить\"");
+                    String answer = scanner.next();
+                    if (answer.equalsIgnoreCase("Завершить")) {
+                        break;
+                    }
+                } else {
+                    System.out.println("Ошибка! Цена товара может быть только положительной! Попробуйте снова.");
+                    goods = goods.replace("\n" + addProduct,"");
+                }
+            } else {
+                System.out.println("Ошибка! Вы ввели некорректную цену товара! Попробуйте снова");
+                goods = goods.replace("\n" + addProduct,"");
             }
         }
     }
 
     void calculatePayment(int numberOfPersons) {
-        System.out.println("Добавленные товары:\n" + goodsList);
+        System.out.println("Добавленные товары:\n" + goods);
         double sumPerPerson = sum / numberOfPersons;
         String sumToPay = String.format("Сумма к оплате на человека: %.2f", sumPerPerson);
-        int lastNumber = (int)Math.floor(sumPerPerson) % 10;
-        if ((int)Math.floor(sumPerPerson) > 10 && (int)Math.floor(sumPerPerson) < 21) {
-            currency = " рублей.";
-        } else if (sumPerPerson == 1) {
+        int sumToInt = (int)Math.floor(sumPerPerson);
+        int secondLastDigit = (sumToInt % 100) / 10;
+        int lastDigit = sumToInt % 10;
+        if (lastDigit == 1 && secondLastDigit != 1) {
             currency = " рубль.";
-        } else if (lastNumber <= 4 && lastNumber > 1) {
+        } else if (lastDigit <= 4 && lastDigit > 1 && secondLastDigit != 1) {
             currency = " рубля.";
         }else {
             currency = " рублей.";
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,24 @@
 import com.sun.source.tree.WhileLoopTree;
 import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Добрый день! На сколько гостей желаете разделить счет?");
         Scanner scanner = new Scanner(System.in);
         int guestNumber;
         while (true) {
-            guestNumber = scanner.nextInt();
-            if (guestNumber > 1) {
-                System.out.println("Спасибо! Сейчас рассчитаем ваше меню.");
-                break;
+            if (scanner.hasNextInt()) {
+                guestNumber = scanner.nextInt();
+                if (guestNumber > 1) {
+                    System.out.println("Спасибо! Сейчас рассчитаем ваше меню.");
+                    break;
+                } else {
+                    System.out.println("Ошибка! Вы ввели некорректное количество гостей! Пожалуйста, повторите попытку.");
+                }
             } else {
-                System.out.println("Ошибка! Вы ввели некорректное количество гостей! Пожалуйста, повторите попытку.");
+                System.out.println("Ошибка! Вы не ввели число! Перезапустите программу и попробуйте снова.");
+                return;
             }
         }
         Calculator calculator = new Calculator();
