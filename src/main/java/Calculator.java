dd8fced59469e4a8e240f2c403e55ccc40271372
Loneliness <costya.kisurin@yandex.ru>
2023-10-15 18:15:19 +0300

    I've created a class 'Calculator'. Implemented in it some methods necessary for solving the problem. In the Main class I added the necessary logic.

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Calculator {
+    private final int countOfPeople;
+    private double totalSum;
+    private final ArrayList<String> titlesOfAddedProducts = new ArrayList<>();
+
+    Calculator(int countOfPeople) {
+        this.countOfPeople = countOfPeople;
+    }
+
+    public int getCountOfPeople() {
+        return countOfPeople;
+    }
+
+    public double getTotalSum() {
+        return totalSum;
+    }
+
+    public void addToTotalPrice(double priceOfProduct) {
+        totalSum += priceOfProduct;
+    }
+
+    public void addTitleOfProduct(String titleOfProduct) {
+        titlesOfAddedProducts.add(titleOfProduct);
+    }
+
+    private void printAllAddedProducts() {
+        System.out.println("Добавленные товары:");
+        for (String titleOfProd : titlesOfAddedProducts) {
+            System.out.println(titleOfProd);
+        }
+    }
+
+    //Function for splitting the bill
+    public void calculate() {
+        double sumPerPerson = totalSum / countOfPeople;
+        String strTemplate = "Сумма на одного человека: %.2f %s.";
+
+        printAllAddedProducts();
+        System.out.format(strTemplate, sumPerPerson, getCorrectWordEnd((int)sumPerPerson % 10));
+    }
+
+    private String getCorrectWordEnd(int currentValueOfSum) {
+        if (currentValueOfSum == 1) return "рубль";
+        else if (currentValueOfSum > 1 && currentValueOfSum < 5) return "рубля";
+        else if (currentValueOfSum == 0 || currentValueOfSum > 4 && currentValueOfSum <= 9) {
+            return "рублей";
+        }
+
+        return "error";
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,46 @@
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int countOfGuests;
+
+        //(Part 1) Endless loop to check the correctness of the entered number of guests
+        while(true) {
+            System.out.println("На сколько человек необходимо разделить счет?");
+
+            if (scanner.hasNextInt()) { countOfGuests = scanner.nextInt(); }
+            else {
+                System.out.println("Ошибка! Вы ввели не целое число. Пожалуйста, попробуйте снова.\n");
+                continue;
+            }
+
+            if (countOfGuests > 1) {
+                break;
+            }
+            else {
+                System.out.println("Ошибка! Было введено некорректное кол-во гостей.\nПожалуйта, попробуйте снова.\n");
+            }
+        }
+
+        /*(Part 2) The part with creating a new instance of the "Calculator" class
+         and inputting the product name and price from the user */
+        Calculator calc = new Calculator(countOfGuests);
+
+        while(true) {
+            System.out.print("Введите название товара: ");
+            calc.addTitleOfProduct(scanner.next());
+            System.out.print("Введите стоимость товара в формате \"рубли.копейки\": ");
+            calc.addToTotalPrice(scanner.nextDouble());
+            System.out.println("Хотите ли добавить еще один товар?\n(Введите 'Завершить', чтобы завершить добавление товаров)");
+            String usersAnswer = scanner.next();
+
+            if (usersAnswer.equalsIgnoreCase("завершить")) {break;}
+        }
+
+        scanner.close();
+
+        // (Part 3) Output of calculation results to the console
+        calc.calculate();
     }
 }
