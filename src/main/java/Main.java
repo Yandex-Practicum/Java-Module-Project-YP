edc3b9ab81dcba8f6c152eb0feb06ec1151b9760
RURURuina <ryuko.matoy.99@bk.ru>
2023-10-18 07:24:05 +0300

    Домашнее задание

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("На скольких человек необходимо разделить счет?");
+        int howMany;
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+                howMany = scanner.nextInt();
+                if (howMany > 1) {
+                    break;
+                } else {
+                    System.out.println("Ошибка! Введите число больше чем 1:");
+                }
+            } else {
+                scanner.nextLine();
+                System.out.println("Извините, но это явно не число. Повторите ввод:");
+            }
+        }
+        Calculator.calculate(howMany);
+        scanner.close();
     }
 }
+
+
