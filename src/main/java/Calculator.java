9a15c172839b969446e6f49314c0ad33afc41110
Anastasya Bochorishvili <nastya@MacBook-Pro-Anastasya.local>
2023-11-20 21:43:10 +0300

    2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,7 +3,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
-    private int NumberOfGuests;
+    private int numberOfGuests;
     private double totalCost;
     private Scanner scanner;
     ArrayList<String> itemNames = new ArrayList<String>();
@@ -21,16 +21,14 @@ public class Calculator {
     private void getNumberOfGuests() {
         System.out.println("Введите количество гостей");
         while (true) {
-            try {  NumberOfGuests = scanner.nextInt();
-                if (NumberOfGuests == 1) {
-                    System.out.println("Некорректное количество гостей");
-                } else if (NumberOfGuests < 1) {
+            try {
+                numberOfGuests = scanner.nextInt();
+                if (numberOfGuests <= 1) {
                     System.out.println("Некорректное количество гостей");
                 } else {
                     break;
                 }
-            }
-            catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("Некорректный ввод. Введите число");
                 scanner.next();
             }
@@ -38,38 +36,46 @@ public class Calculator {
     }
 
     private void addItemsToCalculator() {
-        System.out.println("Добавление товаров для подсчета. Введите 'Завершить' что бы завершить процесс.");
+        System.out.println("Добавление товаров для подсчета.");
         while (true) {
-            System.out.println("Введите название товара:");
+            System.out.println("Введите название товара или Введите 'Завершить', чтобы закончить ввод.");
             String itemName = scanner.next();
             if (itemName.equalsIgnoreCase("Завершить")) {
                 break;
             }
             System.out.println("Введите стоимость товара:");
-            double itemCost = scanner.nextDouble();
-            if (itemCost < 0) {
-                System.out.println("Некорректный ввод.Стоимость не может быть отрицательной");
-            } else{
-                itemNames.add(itemName);
-                totalCost += itemCost;
-                System.out.println("Товар '" + itemName + "' успешно добавлен");
+            while (true) {
+                try {
+                    double itemCost = scanner.nextDouble();
+                    if (itemCost <= 0) {
+                        System.out.println("Некорректный ввод.Стоимость не может быть отрицательной, либо равняться нулю.");
+                    } else {
+                        itemNames.add(itemName);
+                        totalCost += itemCost;
+                        System.out.println("Товар '" + itemName + "' успешно добавлен");
+                        break;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Некорректный ввод.Введите число");
+                    scanner.next();
+                }
             }
         }
     }
 
     private void showResults() {
         System.out.println("Добавленые товары:");
-        for (String name : itemNames){
+        for (String name : itemNames) {
             System.out.println(name);
         }
         System.out.println("Общая стоимость:" + totalCost);
-        int costPerGuest = (int)(totalCost/NumberOfGuests);
+        int costPerGuest = (int) (totalCost / numberOfGuests);
         String currency = "рублей";
-        if (costPerGuest % 10 == 1 && costPerGuest % 100 != 11){
+        if (costPerGuest % 10 == 1 && costPerGuest % 100 != 11) {
             currency = "рубль";
-        } else if (costPerGuest % 10 >= 2 && costPerGuest % 10 <= 4 && (costPerGuest % 100 < 10 || costPerGuest % 100 >= 20)){
+        } else if (costPerGuest % 10 >= 2 && costPerGuest % 10 <= 4 && (costPerGuest % 100 < 10 || costPerGuest % 100 >= 20)) {
             currency = "рубля";
         }
-        System.out.println("Стоимость на каждого гостя: " + String.format("%.2f", totalCost / NumberOfGuests) + ""+ currency );
+        System.out.println("Стоимость на каждого гостя: " + String.format("%.2f", totalCost / numberOfGuests) + " " + currency);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
         calculator.run();
     }
