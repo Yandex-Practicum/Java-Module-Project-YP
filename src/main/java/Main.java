2a47fc90d676d22801173ae7405439f03a38b123
GrafStory <grafstory@yandex.ru>
2023-11-20 20:07:12 +0300

    1) Исправил ошибку ввода кол-ва персон. -Теперь при ввводе буквы будет выводится ошибку. Использовал метод try-catch. 2) Исправил окончани слово "рубль" и "рублей" при подсчете разделение счета 3) Добавил class BillSplitter, чтобы код был не в одном классе(не только в классе Main)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,45 @@
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+
+
+class BillSplitter {
+    private static double roundToTwoDecimalPlaces(double value) {
+        return Math.floor(value * 100) / 100;
+    }
+
+    private static String formatPrice(double value, String currency) {
+        int remainder10 = (int) value % 10;
+        int remainder100 = (int) value % 100;
+        if (remainder10 == 1 && remainder100 != 11) {
+            return String.format("%.2f рубль\n", value);
+        } else if (remainder10 >= 2 && remainder10 <= 4 && (remainder100 < 10 || remainder100 >= 20)) {
+            return String.format("%.2f рубля\n", value);
+        } else {
+            return String.format("%.2f рублей\n", value);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numOfPerson = 0;
         //Ввод кол-ва персон
         while (true) {
-            System.out.print("Укажите количетсво персон для разделения счета: ");
-            numOfPerson = scanner.nextInt();
+            System.out.print("Укажите количество персон для разделения счета: ");
 
-            if (numOfPerson == 1) { // кол-во персон равно 1
-                System.out.println("Ошибка: нельзя разделить счет на одного человека.");
-            }
-            if (numOfPerson < 1) { // кол-во персон равно меньше 1
-                System.out.println("Ошибка: нельзя указать количесвто персон меньше одного.");
-            } else {
-                break;
+            try {
+                numOfPerson = scanner.nextInt();
+
+                if (numOfPerson == 1) {
+                    System.out.println("Ошибка: нельзя разделить счет на одного человека.");
+                } else if (numOfPerson < 1) {
+                    System.out.println("Ошибка: нельзя указать количество персон меньше одного.");
+                } else {
+                    break;
+                }
+            } catch (InputMismatchException exception) {
+                System.out.println("Ошибка: введите целое число.");
+                scanner.next();
             }
         }
 
@@ -27,7 +50,7 @@ public class Main {
         while (true) {
             System.out.println("Введите название товара (или 'Завершить' для выхода): ");
             String names = scanner.next();
-            if (names.equalsIgnoreCase("Завершить")) { // выход из бесконечного цикла при вводе команда "Заверщить"
+            if (names.equalsIgnoreCase("Завершить")) {
                 break;
             }
             System.out.println("Введите цену товара: ");
@@ -41,16 +64,11 @@ public class Main {
         // Вывод результатов
         System.out.println("Добавленные товары:\n" + name);
 
-        double pricePerPerson = price / numOfPerson;
+        double pricePerPerson = roundToTwoDecimalPlaces(price / numOfPerson);
 
-        if (pricePerPerson % 2 == 0) {
-            System.out.printf("Каждый человек должен заплатить: %.2f рублей\n", pricePerPerson);
-        } else {
-            System.out.printf("Каждый человек должен заплатить: %.2f рубля\n", pricePerPerson);
-        }
+        System.out.print("Каждый человек должен заплатить: ");
+        System.out.println(formatPrice(pricePerPerson, "рубль"));
 
         scanner.close();
     }
 }
-
-
