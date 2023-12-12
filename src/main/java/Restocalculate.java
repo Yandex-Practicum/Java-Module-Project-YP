173f546f1eea29b761dddee248c24dd2adb46984
AlexBaranov23 <alexbaranov23@gmail.com>
2023-12-12 17:18:05 +0300

    finish version11

--- src/main/java/Restocalculate.java
+++ src/main/java/Restocalculate.java
@@ -81,48 +81,7 @@ public class Restocalculate {
     }
 }
 
-class Product {
-    private final String name;
-    private final double price;
 
-    public Product(String name, double price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public String toString() {
-        return name + ": " + AmountFormatter.format(price);
-    }
-}
-
-class AmountFormatter {
-    public static String format(double amount) {
-        int lastDigit = (int) amount % 10;
-        int lastTwoDigits = (int) amount % 100;
-
-        String format = "%.2f рублей";
-
-        if (lastTwoDigits >= 11 && lastTwoDigits <= 14) {
-            return String.format(format, amount);
-        } else {
-            switch (lastDigit) {
-                case 1:
-                    format = "%.2f рубль";
-                    break;
-                case 2:
-                case 3:
-                case 4:
-                    format = "%.2f рубля";
-                    break;
-            }
-            return String.format(format, amount);
-        }
-    }
-}
 
 
 
