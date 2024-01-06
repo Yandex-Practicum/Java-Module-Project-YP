dd97ee08c5a631d572602572cc5ebf0c30586dd7
Olga_Rum <olga.tyurenkova@mail.ru>
2024-01-06 11:18:08 +0300

    Create console application №1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calculator = new Calculator();
+        int numberOfPeople = calculator.getTheQuantity();
+        String list = calculator.inputName(numberOfPeople);
+        System.out.println("Количество человек: " + numberOfPeople);
+        System.out.println(list);
     }
 }
