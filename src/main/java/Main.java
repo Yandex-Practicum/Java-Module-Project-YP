fb10922e71a69f7d827ac666bace30b89fd3ef51
Puwk1 <uhanovnikita649@gmail.com>
2024-01-17 15:45:56 +0300

    я не понимаю как после этой ошибки заново запрашивать название продукта, я 3 раза переписал весь код запроса названия и цены и ничего не понимаю.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,63 +6,79 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<String> productName = new ArrayList<>();
+        ArrayList<String> productPrice = new ArrayList<>();
 
+        int company = 0;
         double summ = 0;
-        List productName = new ArrayList<>();
-        List productPrice = new ArrayList<>();
+        double priceOnePerson = 0.00;
 
-        int company = Meet.companyInput();
-        if (company == 1) {
-            System.out.println("Нет смысла ничего считать и делить.");
 
-        } else if (company < 1) {
-            System.out.println("Некорректное значение для подсчёта.");
-
-        } else {
-
-            while (true) {
-                System.out.println("Количество людей: " + company);
-                break;
-            }
-
-            while (true) {
-                System.out.println("Введите название продукта или 'Завершить'. ");
-                String prName = scanner.next();
-                if (prName.equalsIgnoreCase("Завершить")) {
-                    break;
-                } else if (prName.matches(".*\\d.*")) {
-                    System.out.println("Ошибка! Строка содержит цифры.");
-                    return;
+        while (true) {
+            System.out.println("Введите количество гостей");
+            try {
+                company = Integer.parseInt(scanner.nextLine());
+                if (company <= 0) {
+                    System.out.println("Введено некорректное число гостей. Повторите ввод.");
+                } else if (company == 1) {
+                    System.out.println("Нет смысла ничего считать и делить.");
                 } else {
-                    productName.add(prName);
+                    break;
                 }
+            } catch (NumberFormatException e) {
+                System.out.println("Введено не число. Повторите ввод.");
+            }
+        }
 
-                System.out.println("Введите цену продукта");
-                while (!scanner.hasNextDouble()) {
-                    System.out.println("Необходимо ввести число!");
-                    scanner.nextLine();
+        while (true) {
+            System.out.println("Количество людей: " + company);
+            break;
+        }
+        String prName = "";
+        boolean f = false;
+            while (!f) {
+                System.out.println("Введите название продукта или 'Завершить'. ");
+                prName = scanner.nextLine();
+
+                if (prName.equalsIgnoreCase("завершить")) {
+                    break;
                 }
+                if (prName.matches(".*\\d+.*") || prName.trim().isEmpty()) {
+                    System.out.println("Название продукта не может содержать цифры или быть пустым. Пожалуйста, введите название продукта:");
+                    f = true;
 
-                double prPrice = scanner.nextDouble();
-                scanner.nextLine();
+                    //я не понимаю как после этой ошибки заново запрашивать название продукта, я 3 раза переписал весь код запроса названия и цены и ничего не понимаю.
+
+                }
+                productName.add(prName);
+
+
+
+                double prPrice = 0;
+
+                while (prPrice <= 0) {
+                    try {
+                        System.out.println("Введите цену продукта");
+                        prPrice = Double.parseDouble(scanner.nextLine());
+                        if (prPrice <= 0) {
+                            System.out.println("Цена продукта должна быть больше нуля. Пожалуйста, введите положительное число");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Ошибка! Пожалуйста, введите положительное число:");
+                    }
+                }
                 productPrice.add(prPrice + Okonch.getAddition(prPrice));
-
-                double a = summ;
-                summ = a + prPrice;
+                summ += prPrice;
             }
 
 
-            double priceOnePerson = summ / company;
-            String formattedPrice = String.format("%.2f", priceOnePerson);
-
-            System.out.println("Цена с 1-го человека: " + (formattedPrice) + Okonch.getAddition(priceOnePerson));
-            System.out.println("Добавленные товаров:");
-            for (int i = 0; i < productName.size(); i++) {
-                System.out.println(productName.get(i) + " - " + productPrice.get(i));
-
-            }
+        priceOnePerson = summ / company;
+        String formattedPrice = String.format("%.2f", priceOnePerson);
+        System.out.println("Цена с 1-го человека: " + (formattedPrice) + Okonch.getAddition(priceOnePerson));
+        System.out.println("Добавленные товаров:");
+        for (int i = 0; i < productName.size(); i++) {
+            System.out.println(productName.get(i) + " - " + productPrice.get(i));
         }
     }
 }
-
 
--- src/main/java/Meet.java
+++ src/main/java/Meet.java
@@ -1,14 +1,1 @@
-import java.util.Scanner;
-
-public class Meet {
-    public static int companyInput() {
-        System.out.println("Введите количество гостей");
-        Scanner scanner = new Scanner(System.in);
-        while (!scanner.hasNextInt()) {
-            System.out.println("Необходимо ввести число!");
-            scanner.nextLine();
-        }
-        return scanner.nextInt();
-    }
-}
 
