dd2fcc02d409c7887b3069a161933f15f8586788
PayukMax <payukmax@gmail.com>
2024-02-15 18:17:26 +0700

    Обновленная работа. Изначально, на основании прошлых работ, не расчитывал что придется ставить "защиты от дурака" в виде символов при запросе числа и т.д. Ранее когда в задачах такие проверки ставил - робот задачу не пропускал. Все ранее озвученные замечания поправлены...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 public class Main {
     public static void main(String[] args) {
 
@@ -10,7 +11,18 @@ public class Main {
 
         while (true) {
             System.out.println("На скольких распределяем счет? :");
-            person=scanner.nextInt();
+            // принимаем ввод и проверяем его корректность....
+            while (true) {
+                try {
+                    person = scanner.nextInt();
+                    break;
+                } catch (InputMismatchException ex) {
+                    System.err.println("Некорректный ввод, нужно ввести число...");
+                    scanner.nextLine();  //  <----- advance the scanner
+                }
+            }
+            // конец проверки
+            //person=scanner.nextInt();
             if (person<2) {
                 if (person==1) {System.out.println("На одного делить бессмысленно...");}
                 else {System.out.println("Это не серьезно....");}
@@ -42,7 +54,18 @@ static class Bill{
                                 System.out.println("Наименование товара :");
                                 String tNam = scanner.nextLine();
                                 System.out.println("Стоимость :");
-                                float tCost=scanner.nextFloat();
+                                float tCost;
+                                // Далее нужна проверка на корреткность ввода в принципе и на отрицательное число
+                                 while (true) {
+                                       try {
+                                            tCost=scanner.nextFloat();
+                                            if (tCost>0) break; else System.out.println("Стоимость не может быть отрицательной...");
+                                            } catch (InputMismatchException ex) {
+                                                System.out.println("Некорректный ввод, нужно ввести число...");
+                                                scanner.nextLine();
+                                                    }
+                                            }
+
                                 scanner.nextLine();
                                 addTovar(tNam,tCost);}
         }
@@ -58,18 +81,23 @@ static class Bill{
     void calcCost(int p) {
         System.out.println("Вывожу список приобретенного:");
         float totalCost=0.00f;
+        String messTemp;
         for (int i=0; i<listTovar.size(); i++) {
             Tovar tempTovar = listTovar.get(i);
             totalCost=totalCost+tempTovar.cost;
-            System.out.println("Позиция "+i+ " наименование "+tempTovar.name+" стоимость "+tempTovar.cost);
+            messTemp="Позиция "+i+ " наименование "+tempTovar.name+" стоимость %.2f "+Formatter((int)tempTovar.cost);
+            System.out.println(String.format(messTemp,tempTovar.cost));
         }
-        System.out.println("Итого приобретено на сумму : "+totalCost+Formatter((int)totalCost));
-        String messTemp="Сумма на каждого %.2f";
+        messTemp="Итого приобретено на сумму : %.2f"+Formatter((int)totalCost);
+        System.out.println(String.format(messTemp,totalCost));
+
+        messTemp="Сумма на каждого %.2f";
         float costPers = totalCost/p;
         System.out.println(String.format(messTemp,costPers)+" "+Formatter((int)costPers));
     }
 
     String Formatter (int i) {
+        if ((i==11)||(i==12)||(i==13)||(i==14)) return " рублей";
         int tI = i - (i / 10) * 10;
         switch (tI) {
             case 1:
@@ -82,5 +110,7 @@ static class Bill{
                 return " рублей";
         }
     }
+
+
     }
 }
